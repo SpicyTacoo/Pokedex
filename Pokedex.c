2a276ebdf11bc34bfdfb639b7ce3c3bf73dc785f
Benson Yao <99565376+SpicyTacoo@users.noreply.github.com>
2024-03-26 03:43:22 +0800

    Remove comments

--- Pokedex.c
+++ Pokedex.c
@@ -1,11 +1,3 @@
-/*********************************************************************************************************
-This is to certify that this project is my own work, based on my personal efforts in studying and applying the concepts
-learned.  I have constructed the functions and their respective algorithms and corresponding code by myself.  The
-Program was run, tested, and debugged by my own efforts.  I further certify that I have not copied in part or whole or
-otherwise plagiarized the work of other students and/or persons.
-                                                                                                Benson Yao, DLSU ID# 12121452
-*********************************************************************************************************/
-
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
